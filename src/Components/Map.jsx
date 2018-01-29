18215026641022c4499ed92d63c6d5cbe455f84b
gabibatista <gabriela.nerybatista@gmail.com>
2018-01-29 11:15:03 -0200

    Showing a base map.

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,13 +1,28 @@
-import React, {
-    Component
-  } from 'react';
+import React, { Component } from 'react';
+import {withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { compose, withProps } from "recompose";
 
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAzqXIMAGTfkI1FriE3eQ75HsbeuQv1hE8&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `88.5vh` }} />,
+    containerElement: <div style={{ height: `88.5vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={17} defaultCenter={{ lat: -22.814470, lng: -47.044972 }}>
+    <Marker position={{ lat: -22.814470, lng: -47.044972 }} />
+  </GoogleMap>
+));
 
 class Map extends Component {
     render() {
       return (
         <div>
-          <h1>Map</h1>
+          <MyMapComponent isMarkerShown/>
         </div>
       );
     }
