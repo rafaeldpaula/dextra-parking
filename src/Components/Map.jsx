8ce12e42c3cfadf7086f70830e47408d86aa88ba
rodolfogentil <rodolfo.gentil@gmail.com>
2018-01-29 12:03:27 -0200

    Draggable marker with lat and long

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -2,28 +2,46 @@ import React, { Component } from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import { compose, withProps } from "recompose";
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAzqXIMAGTfkI1FriE3eQ75HsbeuQv1hE8&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `88.5vh` }} />,
-    containerElement: <div style={{ height: `88.5vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={17} defaultCenter={{ lat: -22.814470, lng: -47.044972 }}>
-    <Marker position={{ lat: -22.814470, lng: -47.044972 }} />
-  </GoogleMap>
-));
+class MyMapComponent extends Component {  
+  
+  markerOnDragEnd (e, self) {
+    console.log("Toaqui");
+    console.log(e.latLng.lat() + "," + e.latLng.lng());
+ 
+    self.props.location.lat = e.latLng.lat();
+    self.props.location.lng = e.latLng.lng();
+  }
+  
+  render(props) {
+
+    var self = this;
+
+    var ret = compose(
+      withProps({
+        googleMapURL:
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyAzqXIMAGTfkI1FriE3eQ75HsbeuQv1hE8&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `88.5vh` }} />,
+        containerElement: <div style={{ height: `88.5vh` }} />,
+        mapElement: <div style={{ height: `100%` }} />
+      }),
+      withScriptjs,
+      withGoogleMap
+    )(props => (
+      <GoogleMap defaultZoom={17} defaultCenter={{ lat: -22.814470, lng: -47.044972 }}>
+        <Marker position={{ lat: -22.814470, lng: -47.044972 }} 
+                draggable={true}
+                onDragEnd={(e) => this.markerOnDragEnd(e, self)}/>
+      </GoogleMap>
+    ));
+
+    return ret(props);
+  }
+} 
 
 class Map extends Component {
     render() {
       return (
-        <div>
-          <MyMapComponent isMarkerShown/>
-        </div>
+        <MyMapComponent location={{}} isMarkerShown/>
       );
     }
   }
