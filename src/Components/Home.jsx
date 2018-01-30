06699ddbaf4ca6a8cca49dba6a2ff45cb90f27c4
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2018-01-30 11:05:18 -0200

    SelecionarModal

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,7 @@ import React, {
 
 import Map from './Map.jsx';
 import DevolverModal from './DevolverModal.jsx';
+import SelecionarModal from './SelecionarModal.jsx';
 
 import '../styles/FloatingButton.css';
 
@@ -26,9 +27,11 @@ class Home extends Component {
         <Map cars={this.state.cars}/>
         <button type="button" 
                 className="btn btn-outline-primary floating-button top-floating-button"
-                data-toggle="modal" data-target="#selectionar-modal">
+                data-toggle="modal" data-target="#selecionar-modal">
             Selecionar carro
         </button>
+
+        <SelecionarModal items={this.state.cars}/>
         <button type="button" 
                 className="btn btn-outline-success floating-button bottom-floating-button"
                 data-toggle="modal" data-target="#devolver-modal">
