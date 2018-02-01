866bfc69559b7a27e3f4597bdf7074df553e2a49
MrDallOca <tdalloca@gmail.com>
2018-02-01 10:54:25 -0200

    Login got style

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -14,69 +14,68 @@ var config = {
 };
 
 const uiConfig = {
-   signInFlow: "popup",
-   signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-   callbacks: {
-       signInSuccess: () => false
-   }
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+        signInSuccess: () => false
+    }
 };
 
 firebase.initializeApp(config);
 
 var loggedIn = false;
 
-export function isLoggedIn () {
-     return loggedIn;
+export function isLoggedIn() {
+    return loggedIn;
 }
 
 export class Login extends Component {
-   constructor(props) {
-       super(props);
-       this.state = {};
-   }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-   componentDidMount() {
-       firebase.auth().onAuthStateChanged(user => {
-           if (user) {
-               const email = user.email;
-               const name = user.displayName || email;
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                const email = user.email;
+                const name = user.displayName || email;
 
-               user.getIdToken().then(idToken => {
-                   const login = { email, name, idToken };
-                   console.log('Logged in!', login);
-                   localStorage.setItem('login_data', JSON.stringify(login));
-                   this.setState({ login });
-                   fetch('https://dextra-parking-dev.appspot.com/api/cars', {
-                   method: 'GET',
-                   headers: {
-                           'Authorization': 'Bearer ' + login.idToken
-                       }
-                   }).then(c => { 
-                        console.log(c);  
-                        loggedIn = true; 
+                user.getIdToken().then(idToken => {
+                    const login = { email, name, idToken };
+                    console.log('Logged in!', login);
+                    localStorage.setItem('login_data', JSON.stringify(login));
+                    this.setState({ login });
+                    fetch('https://dextra-parking-dev.appspot.com/api/cars', {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + login.idToken
+                        }
+                    }).then(c => {
+                        console.log(c);
+                        loggedIn = true;
                         this.props.history.push("/");
                     });
-               });
-           } else {
-               console.error('Couldn\'t login!');
-           }
-       });
-   }
+                });
+            } else {
+                console.error('Couldn\'t login!');
+            }
+        });
+    }
 
-   render() {
-       return (
-       <div className="App">
-           <header className="App-header">
-                <h1 className="App-title"
-                    style={{textAlign: "center"}}>
-                    Dextra Park
-                </h1>
-           </header>
-           <div className="App-intro">
-               <p>Please sign in</p>
-               <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-           </div>
-       </div>
-       );
-   }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title"
+                        style={{ textAlign: "center" }}>
+                        Dextra Park
+                    </h1>
+                    <div className="App-intro">
+                            <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+                        </div>
+                </header>
+            </div>
+        );
+    }
 }
