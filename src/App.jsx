c257c8dc55b09167b28887fabacd149514352690
MrDallOca <tdalloca@gmail.com>
2018-02-01 10:26:26 -0200

    Login successfully implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import React, {
   Component
 } from 'react';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from 'react-router-dom'
+
 import './styles/App.css';
 import Home from './Components/Home.jsx';
+import { isLoggedIn, Login } from './Components/Login';
 import yawp from 'yawp';
 
 class App extends Component {
@@ -16,9 +23,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" render={() => (
+            isLoggedIn() ? (<Home />) : (<Redirect to="/Login" />))} />
+          <Route path="/Login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
