2f987d67c84e92ac2d99ac25a6b3524f56c40b59
MrDallOca <tdalloca@gmail.com>
2018-02-05 10:48:24 -0200

    commented groundoverlay

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -118,11 +118,15 @@ class MyMapComponent extends Component {
           maxZoom: 25,
           minZoom: 17
         }}>
-        <GroundOverlay
+        {/* <GroundOverlay
           defaultUrl={window.overlayUrl()}
           defaultBounds={window.overlayBounds()}
 
-          defaultOpacity={5} />
+          defaultOpacity={5} /> 
+          
+          RODOLFO: descomenta isso quando estiver pronto
+
+          */}
         {this.makeMarkers()}
 
       </GoogleMap>
