94081e54fbcd6da92aa70211ca64b1f8a7ddf4ce
gabibatista <gabriela.nerybatista@gmail.com>
2018-02-06 13:50:04 -0200

    Delete Geolocation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -138,7 +138,7 @@ class Home extends Component {
                     onClick={() => this.sendLocationUpdate()}>
                     SALVAR
                   </button>
-                  <button type="button"
+                  {/* <button type="button"
                     className="location-button"
                     onClick={() => {
                       if (navigator.geolocation) {
@@ -168,7 +168,7 @@ class Home extends Component {
                       }
                     }}>
                     <i className="fas fa-crosshairs"></i>
-                  </button>
+                  </button>*/}
                 </div>
               )
           })()
